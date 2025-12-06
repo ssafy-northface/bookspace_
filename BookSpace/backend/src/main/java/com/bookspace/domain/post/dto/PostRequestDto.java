a673d5b81749d23e9316eb1c2d5eda780d8d8211
9imDohee <kdhwh09@gmail.com>
2025-12-07 00:25:51 +0900

    [Feat] bookSearch 메서드 분리 (DB 조회용, 알라딘 api 조회용) 및 유저 포스트 작성 시 서버 로직 구현 (DB내 없는 책 추가) (#41)

--- BookSpace/backend/src/main/java/com/bookspace/domain/post/dto/PostRequestDto.java
+++ BookSpace/backend/src/main/java/com/bookspace/domain/post/dto/PostRequestDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class PostRequestDto {
     private long userId;
-    private long bookId;
+    private String isbn; // 유저가 DB내 없는 책으로 게시글을 사용할 경우
     private String postTitle;
     private String postContent;
 }
