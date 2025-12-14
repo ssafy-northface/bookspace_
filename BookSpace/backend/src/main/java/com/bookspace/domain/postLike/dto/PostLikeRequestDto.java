60cd25c069d4fba17367e20d053632b6bd35ef32
9imDohee <kdhwh09@gmail.com>
2025-12-15 02:57:12 +0900

    [BE_Edit] post, postLike 도메인 userId -> 토큰 수정 및 cors  설정 (SecurityConfig) (#79)

--- BookSpace/backend/src/main/java/com/bookspace/domain/postLike/dto/PostLikeRequestDto.java
+++ BookSpace/backend/src/main/java/com/bookspace/domain/postLike/dto/PostLikeRequestDto.java
@@ -2,8 +2,11 @@ package com.bookspace.domain.postLike.dto;
 
 import lombok.Data;
 
+/**
+ * 좋아요 토글(post/delete)
+ * - 유저 id 대신 토큰값
+ */
 @Data
 public class PostLikeRequestDto {
-    private Long userId;
     // postID(PathVariable)
 }
