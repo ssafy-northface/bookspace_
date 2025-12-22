890b84ed306ba7c360ff494e6d7e871bdab82655
seoyeong5864 <wendy2281@naver.com>
2025-12-23 03:30:12 +0900

    [feat] AI 도서추천 API 연결 테스트 구현 (#128)

--- BookSpace/backend/src/main/java/com/bookspace/domain/aiRecommendation/dto/AiRecommendationRequestDto.java
+++ BookSpace/backend/src/main/java/com/bookspace/domain/aiRecommendation/dto/AiRecommendationRequestDto.java
@@ -1,4 +1,15 @@
 package com.bookspace.domain.aiRecommendation.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class AiRecommendationRequestDto {
+
+    // 사용자가 입력한 문장
+    private String userRequest;
+
 }
--- BookSpace/backend/src/main/java/com/bookspace/domain/aiRecommendation/service/AiRecommendationService.java
+++ BookSpace/backend/src/main/java/com/bookspace/domain/aiRecommendation/service/AiRecommendationService.java
@@ -1,4 +1,10 @@
 package com.bookspace.domain.aiRecommendation.service;
 
-public class AiRecommendationService {
+import com.bookspace.domain.aiRecommendation.dto.AiRecommendationRequestDto;
+import com.bookspace.domain.aiRecommendation.dto.AiRecommendationResponseDto;
+
+public interface AiRecommendationService {
+
+    AiRecommendationResponseDto getRecommendedBooks(AiRecommendationRequestDto requestDto);
+
 }
